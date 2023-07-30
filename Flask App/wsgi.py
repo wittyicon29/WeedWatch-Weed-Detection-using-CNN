7b8a65009082f71681075425af83bde0d8f2c5e8
Atharva <99320225+wittyicon29@users.noreply.github.com>
2023-07-30 14:37:41 +0530

    Delete Flask App directory

--- Flask App/wsgi.py
+++
@@ -1,7 +0,0 @@
-#Waitress Server to deploy the model to the server with port 5000
-
-from waitress import serve
-from local_deployment_app import app
-
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5000)
