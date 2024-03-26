6d48cea1467f19097c1f5b4dc17ae34b03f8e12e
Atharva <99320225+wittyicon29@users.noreply.github.com>
2024-03-26 13:17:14 +0530

    Corrected the resizing of images

--- CLI app/Preprocess.py
+++ CLI app/Preprocess.py
@@ -4,7 +4,7 @@ import numpy as np
 # Function to preprocess the input image
 def preprocess_image(image_path):
     img = cv2.imread(image_path)
-    img = cv2.resize(img, (128, 128))  # Resize the image to the required input shape
+    img = cv2.resize(img, (512, 512))  # Resize the image to the required input shape
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img / 255.0  # Normalize the pixel values to the range [0, 1]
     return img[np.newaxis, ...]
